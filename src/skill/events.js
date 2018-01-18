28de15ca72e528a656e401626e2a6d59fc5b204d
Mitchell Harris <mitchellkharris@gmail.com>
2018-01-18 11:43:11 -0600

    Put src back in

---
+++ src/skill/events.js
@@ -0,0 +1,32 @@
+'use strict';
+var Reply = require('voxa').Reply
+  , _ = require('lodash')
+;
+
+module.exports = function(skill) {
+
+  skill.onBeforeReplySent(function(request, reply) {
+    var reprompt = reply.msg.reprompt;
+    if (reprompt) {
+      request.session.attributes.reprompt = reprompt;
+      return;
+    }
+    request.session.attributes.reprompt = null;
+  })
+
+
+  skill.onUnhandledState(function(request) {
+    var reprompt = request.session.attributes.reprompt;
+    if (reprompt) { return { ask: reprompt }; }
+    return {reply: 'Errors.BadLaunch'};
+  })
+
+  skill.onError(function onError(request, error) {
+    return new Reply(request,{
+      tell: "Oh no! You're empire was destroyed by a devastating " +
+           _.sample(['fire','earthquake','hurricane','tornado','tsunami','meteor strike','alien invasion','mole men attack','sneeze. It was pretty incredible'])
+          + ". Try again."
+    });
+  })
+
+}
